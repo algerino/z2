6211c8d0df184c1d85d0f9d291210e20f673675a
mohammed amine <c14web@gmail.com>
2012-10-19 11:44:07 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Z2::Application.config.secret_token = '5b3a36d28a28d7525e6b39934c57b298071d121d0b6b7fbd3ecc8352bdf75fca32f67baab58fa947ea5f8adcdee5eed585e123691f5db5d32b6b40dee364117a'
